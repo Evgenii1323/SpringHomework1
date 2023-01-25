bc4c49f5cfb8ee9ab21b1703aa504eeead8ace18
Evgenii Vasilev <bk6789@bk.ru>
2023-01-25 13:54:19 +0700

    Second commit

--- src/main/java/Connection.java
+++ src/main/java/Connection.java
@@ -5,36 +5,38 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Connection implements Runnable {
-    private final BufferedReader IN;
-    private final BufferedOutputStream OUT;
-    private final List<String> VALID_PATHS = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    private final Socket socket;
+    private static final List<String> VALID_PATHS = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
 
-    public Connection(Socket socket) throws IOException {
-        this.IN = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        this.OUT = new BufferedOutputStream(socket.getOutputStream());
+    public Connection(Socket socket) {
+        this.socket = socket;
     }
 
     @Override
     public void run() {
         while (true) {
-            try {
-                String requestLine = IN.readLine();
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())) {
+                String requestLine = in.readLine();
                 String[] parts = requestLine.split(" ");
+
                 if (parts.length != 3) {
                     break;
                 }
+
                 String path = parts[1];
 
                 if (!VALID_PATHS.contains(path)) {
-                    OUT.write((
+                    out.write((
                             "HTTP/1.1 404 Not Found\r\n" +
                                     "Content-Length: 0\r\n" +
                                     "Connection: close\r\n" +
                                     "\r\n"
                     ).getBytes());
-                    OUT.flush();
+                    out.flush();
                     break;
                 }
+
                 Path filePath = Path.of(".", "public", path);
                 String mimeType = Files.probeContentType(filePath);
                 long length = Files.size(filePath);
@@ -51,29 +53,38 @@ public class Connection implements Runnable {
         }
     }
 
-    public void writeClassicHtml(Path filePath, String mimeType) throws IOException {
-        String template = Files.readString(filePath);
-        byte[] content = template.replace("{time}", LocalDateTime.now().toString()).getBytes();
-        OUT.write((
-                "HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: " + mimeType + "\r\n" +
-                        "Content-Length: " + content.length + "\r\n" +
-                        "Connection: close\r\n" +
-                        "\r\n"
-        ).getBytes());
-        OUT.write(content);
-        OUT.flush();
+    public void writeClassicHtml(Path filePath, String mimeType) {
+        try (BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())) {
+            String template = Files.readString(filePath);
+            byte[] content = template.replace("{time}", LocalDateTime.now().toString()).getBytes();
+
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.write(content);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void write(Path filePath, String mimeType, long length) throws IOException {
-        OUT.write((
-                "HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: " + mimeType + "\r\n" +
-                        "Content-Length: " + length + "\r\n" +
-                        "Connection: close\r\n" +
-                        "\r\n"
-        ).getBytes());
-        Files.copy(filePath, OUT);
-        OUT.flush();
+    public void write(Path filePath, String mimeType, long length) {
+        try (BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())) {
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
